038a5fb0327b19db71137c7d727da09f557cc70c
Denys Karlinskyi <fondunndev@gmail.com>
2024-11-07 01:57:09 +0200

    upd config

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
-  basePath: "/nextjs-github-pages",
+  basePath: process.env.DEPLOYED_GITHUB_PATH || "",
   images: {
     unoptimized: true,
   },
