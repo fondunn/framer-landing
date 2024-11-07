59c1dfd266b70ec1f1e2635d07ca7252ff0b3584
Denys Karlinskyi <fondunndev@gmail.com>
2024-11-07 02:24:56 +0200

    rm tw prebuild, restore styles

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
-  basePath: process.env.DEPLOYED_GITHUB_PATH || "",
+  basePath: "/framer-landing",
   images: {
     unoptimized: true,
   },
